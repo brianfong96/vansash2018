90d6d5a3722e24e46641bcc93d301513565acf3b
Brian Fong <brianwkfong@gmail.com>
2018-09-22 13:11:59 -0700

    made it into a function

--- vansash.R
+++ vansash.R
@@ -1,20 +1,29 @@
+plotstat<- function(p, f, a, t, title, xlabel, ylabel)
+{
+  getwd()
+  setwd(p)
+  df <- read.csv(f)
+  t <- df[df$Action==a & df$team==t,]
+  
+  library(ggplot2)
+  ggplot() +
+    geom_point(data = t, aes(x = LocX, y = LocY, colour = Result), size = 3) + 
+    xlim(c(0, 100)) +
+    ylim(c(0, 100)) + 
+    ggtitle(title) +
+    xlab(xlabel) + 
+    ylab(ylabel)
+}
+
 # path and file names
-p <- "C:/Users/Brian/Downloads/" 
-f <- "WFC_Ortec_MatchDate_2018_Datathon.csv"
+pathname <- "C:/Users/Brian/Downloads/" 
+filename <- "WFC_Ortec_MatchDate_2018_Datathon.csv"
+action <- "direct free kick"
+team <- "Portland Timbers"
+gtitle <- "Portland Timber Players Direct Free Kick"
+xl <- "Side line (Shooting this way >>>) "
+yl <- "Goal line"
 
-getwd()
-setwd(p)
-df <- read.csv(f)
-t <- df[df$Action=="direct free kick" & df$team=="Portland Timbers",]
-
-#rm(variablename)
-library(ggplot2)
-ggplot() +
-  geom_point(data = t, aes(x = LocX, y = LocY, colour = Result), size = 3) + 
-  xlim(c(0, 100)) +
-  ylim(c(0, 100)) + 
-  ggtitle("Portland Timber Players Direct Free Kick") +
-  xlab("Side line (Shooting this way >>>) ") + 
-  ylab("Goal line")
+plotstat(p = pathname,f = filename, a=action, t = team, title = gtitle, xlabel = xl, ylabel = yl)
 
   
