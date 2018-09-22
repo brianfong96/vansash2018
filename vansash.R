1482459b528390bbda6695a0e22cd2f890d19c1e
Brian Fong <brianwkfong@gmail.com>
2018-09-22 12:39:00 -0700

    using gg plot

--- vansash.R
+++ vansash.R
@@ -1,19 +1,25 @@
 # path and file names
-p <- "" 
-f <- ""
+p <- "C:/Users/Brian/Downloads/" 
+f <- "WFC_Ortec_MatchDate_2018_Datathon.csv"
 
 # variable to plot
-varx <- ''
-vary <- ''
+varx <- 'LocX'
+vary <- 'LocY'
 
 getwd()
 setwd(p)
 df <- read.csv(f)
-t <- df[queries]
-t
+t <- df[df$Action=="direct free kick" & df$team=="Portland Timbers",]
 t1 <- t[,c(varx, vary)]
-t1
 
-plot(t1)
+plot(t1, col=df$player, xlim=c(0,100), ylim=c(0,100))
 
 #rm(variablename)
+library(ggplot2)
+ggplot() +
+  geom_point(data = t, aes(x = LocX, y = LocY, colour = Result)) + 
+  xlim(c(0, 100)) +
+  ylim(c(0, 100)) + 
+  ggtitle("Portland Timber Players Direct Free Kick")
+
+  
