186709a96c15afcca69fc88bd6b41f7d80f14a84
Brian Fong <brianwkfong@gmail.com>
2018-09-22 12:56:54 -0700

    rescaled size of dots

--- vansash.R
+++ vansash.R
@@ -2,24 +2,19 @@
 p <- "C:/Users/Brian/Downloads/" 
 f <- "WFC_Ortec_MatchDate_2018_Datathon.csv"
 
-# variable to plot
-varx <- 'LocX'
-vary <- 'LocY'
-
 getwd()
 setwd(p)
 df <- read.csv(f)
 t <- df[df$Action=="direct free kick" & df$team=="Portland Timbers",]
-t1 <- t[,c(varx, vary)]
-
-plot(t1, col=df$player, xlim=c(0,100), ylim=c(0,100))
 
 #rm(variablename)
 library(ggplot2)
 ggplot() +
-  geom_point(data = t, aes(x = LocX, y = LocY, colour = Result)) + 
+  geom_point(data = t, aes(x = LocX, y = LocY, colour = Result), size = 3) + 
   xlim(c(0, 100)) +
   ylim(c(0, 100)) + 
-  ggtitle("Portland Timber Players Direct Free Kick")
+  ggtitle("Portland Timber Players Direct Free Kick") +
+  xlab("Side line (Shooting this way >>>) ") + 
+  ylab("Goal line")
 
   
